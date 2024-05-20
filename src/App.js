37c7fde4dad79ae2e55df785cbd04e957803a1d5
Chandresh Patidar <chandreshpatidar5@gmail.com>
2024-05-20 21:13:18 +0530

    Show PDF file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { pdfjs } from 'react-pdf';
+import { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+import pdfFile from './testPdf.pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL('pdfjs-dist/build/pdf.worker.min.js', import.meta.url).toString();
 
 function App() {
+  const [numPages, setNumPages] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Showing PDF using pdf-react</h2>
+      <Document
+        file={pdfFile}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page
+          pageNumber={pageNumber}
+          width={window?.innerWidth / 1.9}
+          renderAnnotationLayer={false}
+          renderTextLayer={false}
+        />
+      </Document>
+
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
     </div>
   );
 }
